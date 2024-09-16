4cf93fd2861da7249e3a5742839cdb0cbe716110
AndreaWalrus <andrea@DESKTOP-I8F6GOQ>
2024-09-16 17:08:22 +0200

    Implemented Queue struct

--- 1.Processi-e-thread/code/concurrent_threads.c
+++ 1.Processi-e-thread/code/concurrent_threads.c
@@ -7,6 +7,75 @@
 #define M 10000 // number of iterations per thread
 #define V 1 // value added to the balance by each thread at each iteration
 
+//Queue Structure
+typedef struct queue_node t_queue_Node;
+
+typedef struct queue{
+	t_queue_Node* Head;
+	t_queue_Node* Tail;
+}t_queue;
+
+struct queue_node{
+	pthread_t* thread;
+	t_queue_Node* next;
+};
+
+//Queue Methods
+t_queue* create_queue(){
+	t_queue* Queue = (t_queue *)malloc(sizeof(t_queue));
+}
+
+void delete_queue(t_queue* Queue){
+	t_queue_Node* Head = Queue -> Head;
+	t_queue_Node* temp=Head;
+	while(temp!=NULL){
+		temp=Head->next;
+		free(Head);
+		Head=Head->next;
+	}
+	free(Queue);
+}
+
+void enqueue(t_queue* Queue, pthread_t* thread){
+	t_queue_Node* Tail = Queue->Tail;
+	if(Tail==NULL){
+		t_queue_Node* temp = (t_queue_Node*)malloc(sizeof(t_queue_Node));
+		temp->thread=thread;
+		temp->next=NULL;
+		Queue->Head=temp;
+		Queue->Tail=temp;
+	}else{
+		t_queue_Node* temp = (t_queue_Node*)malloc(sizeof(t_queue_Node));
+		temp->thread=thread;
+		temp->next=NULL;
+		Tail->next=temp;
+		Queue->Tail=Tail;
+	}
+
+}
+
+pthread_t* dequeue(t_queue* Queue){
+	t_queue_Node* Head = Queue->Head;
+	if(Head==NULL) return NULL;
+	pthread_t* temp=Head->thread;
+	Queue->Head=Head->next;
+	free(Head);
+	return temp;
+}
+
+pthread_t* get_head(t_queue* Queue){
+	if(Queue->Head!=NULL) return Queue->Head->thread;
+	else return NULL;
+}
+
+void print_queue(t_queue* Queue){
+	t_queue_Node* Head = Queue->Head;
+	while(Head!=NULL){
+		printf("id:%ln\n", Head->thread);
+		Head=Head->next;
+	}
+}
+
 unsigned long int shared_variable;
 int n = N, m = M, v = V;
 
@@ -23,6 +92,9 @@ int main(int argc, char **argv)
 	if (argc > 2) m = atoi(argv[2]);
 	if (argc > 3) v = atoi(argv[3]);
 	shared_variable = 0;
+
+	//queue allocation
+
 
 	printf("Going to start %d threads, each adding %d times %d to a shared variable initialized to zero...", n, m, v); fflush(stdout);
 	pthread_t* threads = (pthread_t*)malloc(n * sizeof(pthread_t)); // also calloc(n,sizeof(pthread_t))
