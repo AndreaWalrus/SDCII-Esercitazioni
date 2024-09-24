fe444c918d31f21bd2a408367521a160af88ebe8
AndreaWalrus <andrea@DESKTOP-I8F6GOQ>
2024-09-24 17:06:19 +0200

    Partially implemented 2 riepilogo.c

--- 5.Esercizio-riepilogativo/code/riepilogo.c
+++ 5.Esercizio-riepilogativo/code/riepilogo.c
@@ -41,7 +41,7 @@ int n = N, m = M, t = T;
  #define SEM_MAIN "/mysem_main"
  #define SEM_CHILD "/mysem_child"
 
- sem_t * sem_cs, sem_main, sem_child;
+ sem_t * sem_cs, *sem_main, *sem_child;
 
 /* TODO: declare a shared memory and the data type to be placed 
  * in the shared memory, and choose a unique identifier for
@@ -93,6 +93,30 @@ void destroy_named_semaphore(const char* name){
     printf("done!!!\n");
 }
 
+void initMemory() {
+    shm_unlink(SHM_NAME);
+
+    fd_shm = shm_open(SHM_NAME, O_CREAT | O_EXCL | O_RDWR, 0666);
+    if(fd_shm==-1) handle_error("shm_open error");
+
+    if(ftruncate(fd_shm, sizeof(int)*n*m)!=0) handle_error("ftruncate error");
+
+    data = mmap(0, sizeof(int)*n*m, PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0);
+    if(data==MAP_FAILED) handle_error("mmap error");
+
+    memset(data, 0, sizeof(int)*n*m);
+}
+
+void closeMemory() {
+
+    if(munmap(data, sizeof(int)*n*m)!=0) handle_error("munmap error");
+
+    if(close(fd_shm)!=0) handle_error("close error");
+
+    if(shm_unlink(SHM_NAME)!=0) handle_error("shm_unlink error");
+
+}
+
 
 
 void parseOutput() {
@@ -136,16 +160,23 @@ void* thread_function(void* x) {
     /* TODO: protect the critical section using semaphore(s) as needed */
 
     // open file, write child identity and close file
+
+    printf("[Child#%d-Thread#%d] Waiting\n", args->child_id, args->thread_id);	
+    
+    if(sem_wait(sem_cs)!=0) handle_error("sem_wait cs error");
+
     int fd = open(FILENAME, O_WRONLY | O_APPEND);
     if (fd < 0) handle_error("error while opening file");
     printf("[Child#%d-Thread#%d] File %s opened in append mode!!!\n", args->child_id, args->thread_id, FILENAME);	
 
     write(fd, &(args->child_id), sizeof(int));
     printf("[Child#%d-Thread#%d] %d appended to file %s opened in append mode!!!\n", args->child_id, args->thread_id, args->child_id, FILENAME);	
-
+    
     close(fd);
     printf("[Child#%d-Thread#%d] File %s closed!!!\n", args->child_id, args->thread_id, FILENAME);
 
+    if(sem_post(sem_cs)!=0) handle_error("sem_post cs error");
+    
     free(x);
     pthread_exit(NULL);
 }
@@ -157,6 +188,37 @@ void mainProcess() {
      * to end their activities, and waits for their termination.
      * Finally, it calls the parseOutput() method and releases
      * any shared resources. */
+
+
+    printf("[Main] Waiting for all children to be created\n");
+    while(data[1]!=n){
+        printf("[Main] %d children created\n", data[1]);
+        usleep(100*1000);
+    }
+    data[2]=1;
+    for(int i=0; i<n; i++){
+        if(sem_post(sem_main)!=0) handle_error("sem_post main error");
+    }
+
+    printf("[Main] All children notified, sleeping...\n");
+    sleep(t);
+    printf("[Main] Notifying all children to end\n");
+    data[0]=1;
+
+    for (int i=0; i<n; ++i) {
+        int status;
+        int ret = wait(&status);
+        if (ret == -1) handle_error("wait");
+        if (WEXITSTATUS(status)) handle_error_en(WEXITSTATUS(status), "child crashed");
+    }
+    printf("[Main] All children ended, parsing output...\n");
+
+    parseOutput();
+
+    closeMemory();
+    destroy_named_semaphore(SEM_CS);
+    destroy_named_semaphore(SEM_CHILD);
+    destroy_named_semaphore(SEM_MAIN);
 }
 
 void childProcess(int child_id) {
@@ -169,6 +231,38 @@ void childProcess(int child_id) {
      * notification has arrived, the child process notifies the main
      * process that it is about to terminate, and releases any
      * shared resources before exiting. */
+    
+    printf("[Child#%d] Created\n", child_id);
+    if(sem_wait(sem_child)!=0) handle_error("sem_wait child error");
+    data[1]++;
+    if(sem_post(sem_child)!=0) handle_error("sem_post child error");
+    printf("[Child#%d] Notified Main\n", child_id);
+
+    while(data[2]!=1);
+
+    printf("[Child#%d] Started\n", child_id);
+
+    pthread_t* threads = (pthread_t*)malloc(m * sizeof(pthread_t));
+	int i;
+    while(data[0]!=1){
+        printf("[Child#%d] Spawning %d threads\n", child_id, m);
+        for (i = 0; i < m; i++)
+            thread_args_t* args = (thread_args_t*)malloc(sizeof(thread_args_t));
+            if (pthread_create(&threads[i], NULL, thread_function, NULL) != 0) {
+                fprintf(stderr, "Can't create a new thread, error %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+            printf("%d",i);
+        printf("[Child#%d] Spawned %d threads\n", child_id, m);
+
+        printf("[Child#%d] Waiting for the termination of all the %d threads...\n", child_id , m); fflush(stdout);
+        for (i = 0; i < m; i++)
+            pthread_join(threads[i], NULL);
+        printf("[Child#%d] All threads terminated\n", child_id);
+    }    
+
+    printf("[Child#%d] Terminating from main signal!!!\n", child_id);
+    free(threads);
 }
 
 int main(int argc, char **argv) {
@@ -186,5 +280,23 @@ int main(int argc, char **argv) {
      * memory), then the main process executes function mainProcess() once 
      * all the children have been created */
 
+    sem_cs = create_named_semaphore(SEM_CS, 0660, 1);
+    sem_child = create_named_semaphore(SEM_CHILD, 0660, 1);
+    sem_main = create_named_semaphore(SEM_MAIN, 0660, 0);
+    
+    initMemory();
+
+    int i, ret;
+    for (i=0; i<n; ++i) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            handle_error("fork");
+        } else if (pid == 0) {
+            childProcess(i);
+            _exit(EXIT_SUCCESS);
+        }
+    }
+    
+    mainProcess();
     exit(EXIT_SUCCESS);
 }
